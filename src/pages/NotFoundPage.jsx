0396c28a4efe748b68a42c623d8135b13d2858b1
frontendGuyVth <coding.since21@gmail.com>
2025-03-23 01:00:22 +0530

    feature/adding-routes feature/added-all-file-logic feature/added-not-found-page

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+export default function NotFoundPage() {  
+    return (
+      <>
+        <h1>Page Not Found</h1>
+        <p>The Link you followed is broken, cheack url again, PLEASE!</p>
+      </>
+    );
+  }
+  
