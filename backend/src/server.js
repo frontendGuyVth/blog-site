60a9f6acb313c5a8ec42d530798c14db5076a064
frontendGuyVth <coding.since21@gmail.com>
2025-03-24 19:41:14 +0530

    feature/connected mongodb database with express app feature/updated endpoint for upvotes and comments to update data in db and not in local

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,61 +1,67 @@
 import express from "express";
+import { MongoClient, ServerApiVersion } from "mongodb"
 
 const app = express();
 app.use(express.json());
 
-const articleInfo = [
-  {
-    name: "learn-node",
-    upvotes: 0,
-    comments: [],
-  },
-  {
-    name: "learn-react",
-    upvotes: 0,
-    comments: [],
-  },
-  {
-    name: "mongodb",
-    upvotes: 0,
-    comments: [],
-  },
-];
+let db;
+async function connectToDB() {
+  const uri = 'mongodb://127.0.0.1:27017';
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  })
+  await client.connect();
 
-app.post("/api/articles/:name/upvote", (req, res) => {
-  const article = articleInfo.find((a) => a.name === req.params.name);
-  article.upvotes += 1;
+  db = client.db('blog-site-db');
+}
+
+
+app.get('/api/articles/:name', async (req, res) => {
+  const {name} = req.params
+
+  const article = await db.collection('article').findOne({name});
 
   res.json(article);
+})
+
+app.post("/api/articles/:name/upvote", async (req, res) => {
+
+  const {name} = req.params
+
+  const updatedArticleWithUpvotes = await db.collection('article').findOneAndUpdate({name}, {
+    $inc: {upvotes: 1}
+  }, {
+    returnDocument: "after"
+  })
+
+  res.json(updatedArticleWithUpvotes);
+
 });
 
-app.post("/api/articles/:name/comments", (req, res) => {
+app.post("/api/articles/:name/comments", async (req, res) => {
   const { name } = req.params;
   const { postedBy, text } = req.body;
+  const newComment = {postedBy, text}
 
-  console.log(req.body, "sdbfdshbf");
+  const updatedArticleWithComment = await db.collection('article').findOneAndUpdate({name}, {
+    $push: {comments: newComment}
+  }, {
+    returnDocument: "after"
+  })
 
-  const article = articleInfo.find((a) => a.name === name);
+  res.json(updatedArticleWithComment)
+ 
+});
 
-  article.comments.push({
-    postedBy,
-    text,
+async function start() {
+  await connectToDB()
+  app.listen(8000, () => {
+    console.log("server is listening on port 8000");
   });
+}
 
-  res.json(article);
-});
-
-// app.get("/hello", function (req, res) {
-//   res.send("HELLO! from get");
-// });
-
-// app.get("/hello/:name", function (req, res) {
-//   res.send("HELLO! " + req.params.name + "");
-// });
-
-// app.post("/hello", function (req, res) {
-//   res.send("Hello " + req.body.name + " from post!");
-// });
-
-app.listen(8000, () => {
-  console.log("server is listening on port 8000");
-});
+start();
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,11 @@
-import { Outlet } from 'react-router-dom'
-import NavBar from './NavBar'
+import { Outlet } from "react-router-dom";
+import NavBar from "./NavBar";
 
 export default function Layout() {
-    return (
-        <>
-        <NavBar />
-        <Outlet />
-        </>
-    )
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
 }
